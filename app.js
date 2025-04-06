34f0b8b9cd03e8b32fd5cfa2660e5ecccb03d6cc
Ojonugwa Samuel Adama <evojonugwa@gmail.com>
2025-04-06 11:46:12 -0700

    modified an app.js file added url functions and loop for express framework

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const express = require ('express');
+const express = require('express'); // import express framework
+const path = require('path'); // import the path in-built module
+const ejs = require ('ejs'); // import the ejs module
 
-const PORT = 8000;
+//variable for express function
 const app = express();
 
+// Express's built-in middleware
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+//Directory for Express
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Hello world');
-});
-// app.post();
-// app.put();
-// app.delete();
-
-app.listen(PORT, () => { console.log (`istening on port 8000`)
-
+    res.render('pages/index');
 });
 
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Craftly is running on port ${PORT}`);
+});
